fd1473399f2982ba25584bdb52ebdc3e158aae3d
emerson <emerson100loko@hotmail.com>
2023-04-15 22:05:37 -0300

    excecao com resposta customizada

---
+++ src/main/java/com/devsuperior/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,2 @@
+package com.devsuperior.dscommerce.controllers.handlers;public class ControllerExceptionHandler {
+}
---
+++ src/main/java/com/devsuperior/dscommerce/dto/CustomError.java
@@ -0,0 +1,7 @@
+package com.devsuperior.dscommerce.dto;
+
+public class CustomError {
+
+
+    
+}
