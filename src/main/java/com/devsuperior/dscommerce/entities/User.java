aea2ef103b459f878de8c0ce4ee0d2d9ef9a395f
emerson <emerson100loko@hotmail.com>
2023-05-21 12:12:44 -0300

    User Role model

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -1,10 +1,11 @@
 package com.devsuperior.dscommerce.entities;
 
 import javax.persistence.*;
-
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 
 @Entity
@@ -24,7 +25,11 @@ public class User {
 
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
-
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
     public User(){
     }
 
@@ -87,5 +92,8 @@ public class User {
 
     public List<Order> getOrders() {
         return orders;
+    }
+    public Set<Role> getRoles() {
+        return roles;
     }
 }
