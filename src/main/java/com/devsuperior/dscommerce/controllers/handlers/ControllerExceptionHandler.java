5f5efa4f219928799235cff3f30d7f6c16a725bf
emerson <emerson100loko@hotmail.com>
2023-05-31 10:31:01 -0300

    validate self or admin

--- src/main/java/com/devsuperior/dscommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -3,8 +3,8 @@ package com.devsuperior.dscommerce.controllers.handlers;
 import com.devsuperior.dscommerce.dto.CustomError;
 import com.devsuperior.dscommerce.dto.ValidationError;
 import com.devsuperior.dscommerce.services.exceptions.DataBaseException;
+import com.devsuperior.dscommerce.services.exceptions.ForbiddenException;
 import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
-import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -12,6 +12,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.Instant;
 
     @ControllerAdvice
@@ -39,4 +40,12 @@ import java.time.Instant;
             }
             return ResponseEntity.status(status).body(err);
         }
+
+        @ExceptionHandler(ForbiddenException.class)
+        public ResponseEntity<CustomError> forbidden(ForbiddenException e, HttpServletRequest request) {
+            HttpStatus status = HttpStatus.FORBIDDEN;
+            CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+            return ResponseEntity.status(status).body(err);
+        }
+
     }
--- src/main/java/com/devsuperior/dscommerce/services/OrderService.java
+++ src/main/java/com/devsuperior/dscommerce/services/OrderService.java
@@ -27,10 +27,14 @@ public class OrderService {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private AuthService authService;
+
     @Transactional(readOnly = true)
     public OrderDTO findbyId(Long id) {
         Order order = repository.findById(id).orElseThrow(
                 () -> new ResourceNotFoundException("Recurso nao encontrado"));
+        authService.validateSelfOrAdmin(order.getClient().getId());
         return new OrderDTO(order);
     }
 
